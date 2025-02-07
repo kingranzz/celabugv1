d9e06b88ac802e77f8e24d1f02ee7a418501efe6
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-02-07 18:59:20 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [6142885267], // ganti jadi id mu
+    botToken: "7687205613:AAFGUGM3Gf9ZaKlHjC-3M6otyvbY47ma9WA", //isi make token bot mu
+}
+
+module.exports = global;
