9454097b6c7520f4675b412a89ad4ccc6e897b56
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-02-08 12:50:47 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const global = {
-    owner: [5965707646], // ganti jadi id mu
-    botToken: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU", //isi make token bot mu
-}
-
-module.exports = global;
